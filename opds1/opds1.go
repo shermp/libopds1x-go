3ecb2c99d642cc3ed25fda47e9730550bbfc1a6e
shermp <14854761+shermp@users.noreply.github.com>
2021-03-15 08:32:24 +1300

    Move 'helper methods to main file

--- opds1/opds1.go
+++ opds1/opds1.go
@@ -6,6 +6,7 @@ import (
 	"encoding/xml"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -118,4 +119,30 @@ func ParseResponse(r *http.Response) (*Feed, error) {
 	}
 	xml.Unmarshal(buff, &feed)
 	return &feed, nil
+}
+
+func (f *Feed) detectFeedType() {
+	if !f.feedTypeDetected {
+		f.isNavigation = true
+		for _, entry := range f.Entries {
+			for _, link := range entry.Links {
+				if strings.HasPrefix(link.Rel, "http://opds-spec.org/acquisition") ||
+					link.TypeLink == "application/atom+xml;profile=opds-catalog;kind=acquisition" {
+					f.isNavigation = false
+				}
+			}
+		}
+	}
+}
+
+// IsNavigation determines whether feed is a navigation feed.
+func (f *Feed) IsNavigation() bool {
+	f.detectFeedType()
+	return f.isNavigation
+}
+
+// IsAcquisition determines whether feed is an acquisition feed
+func (f *Feed) IsAcquisition() bool {
+	f.detectFeedType()
+	return !f.isNavigation
 }
